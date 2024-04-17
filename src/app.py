d50c71554fc7295a2c7617c9382febd4f1cbd968
Maikel Guerra Pantoja <154547587+Maikgp@users.noreply.github.com>
2024-04-17 17:36:29 +0000

    en proceso

--- src/app.py
+++ src/app.py
@@ -15,6 +15,34 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+
+Jhon = {
+    "id": jackson_family._generateId(),
+    "name":"John Jackson",
+    "age": "33 Years old",
+    "lucky_numbers": [7, 13, 22]
+}
+jackson_family.add_member(Jhon)
+
+
+Jane = {
+    "id": jackson_family._generateId(),
+    "name":"Jane Jackson",
+    "age":"35 Years old",
+    "lucky_numbers": [10, 14, 3]
+}
+jackson_family.add_member(Jane)
+
+Jimmy = {
+    "id": jackson_family._generateId(),
+    "name": "Jimmy  Jackson",
+    "age": "5 Years old",
+    "lucky_numbers": 1
+}
+jackson_family.add_member(Jimmy)
+
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -25,18 +53,15 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
-
-
+    response_body = members
     return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
